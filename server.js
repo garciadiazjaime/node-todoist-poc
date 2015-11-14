8e552d969b84d065f52ed2aec68badc00b6eb2da
Jaime Garcia <jgarciaqo@gmail.com>
2015-11-14 14:56:18 -0500

    init

---
+++ server.js
@@ -0,0 +1,33 @@
+const todoistHandler = require('./lib/todoistHandler');
+
+var todoist = todoistHandler();
+
+// todoist.getAll().then(function(response) {
+//   console.log('response', response);
+// });
+
+// todoist.addProject('cool').then(function(response) {
+//   console.log('response', response);
+// });
+
+// todoist.getProjects().then(function(response) {
+//   console.log('response', response);
+// });
+
+// todoist.getItems().then(function(response) {
+//   console.log('response', response);
+// });
+
+// project 5 155704829
+// todoist.getItemsFromProject(155704829).then(function(response) {
+//   console.log('response', response);
+// });
+
+// todoist.addItemToProject(155704829, 'lets to this').then(function(response) {
+//   console.log('response', response);
+// });
+
+// 6834463 "nice" from proj5
+// todoist.completeItemsFromProject(155704829, [6834463]).then(function(response) {
+//   console.log('response', response);
+// });
