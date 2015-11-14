7299d91bf358e7b1bd02e14d8977a8d81d69d50c
Jaime Garcia <jgarciaqo@gmail.com>
2015-11-14 15:17:59 -0500

    setup todoist lib

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-const todoistHandler = require('./lib/todoistHandler');
 
-var todoist = todoistHandler();
+
+var todoist = require('require-all')(__dirname + '/lib/todoist');
+
 
 // todoist.getAll().then(function(response) {
-//   console.log('response', response);
+//   console.log('getAll response', response);
 // });
 
-// todoist.addProject('cool').then(function(response) {
-//   console.log('response', response);
+// todoist.addProject('cool 2').then(function(response) {
+//   console.log('addProject response', response);
 // });
 
 // todoist.getProjects().then(function(response) {
-//   console.log('response', response);
+//   console.log('getProjects response', response);
 // });
 
 // todoist.getItems().then(function(response) {
-//   console.log('response', response);
+//   console.log('getItems response', response);
 // });
 
-// project 5 155704829
+// // project 5 155704829
 // todoist.getItemsFromProject(155704829).then(function(response) {
-//   console.log('response', response);
+//   console.log('getItemsFromProject response', response);
 // });
 
-// todoist.addItemToProject(155704829, 'lets to this').then(function(response) {
-//   console.log('response', response);
-// });
+// todoist.addItemToProject(155704829, 'lets to this again')
+//   .then(function(response) {
+//     console.log('addItemToProject response', response);
+//   });
 
-// 6834463 "nice" from proj5
-// todoist.completeItemsFromProject(155704829, [6834463]).then(function(response) {
-//   console.log('response', response);
-// });
+// 6836684 "lets do this again" from proj5
+todoist.completeItemsFromProject(155704829, [6836684]).then(function(response) {
+  console.log('response', response);
+});
